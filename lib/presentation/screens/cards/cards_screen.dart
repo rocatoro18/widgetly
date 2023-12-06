350c100326f435e3720cf460f0de23a29983586f
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-06 12:56:15 -0700

    Tarjetas con border

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -36,6 +36,8 @@ class _CardsView extends StatelessWidget {
           // PASAMOS POR EL MAP PARA REGRESAR UN NUEVO ARREGLO/LISTADO
           ...cards.map((card) =>
               _CardType1(label: card['label'], elevation: card['elevation'])),
+          ...cards.map((card) =>
+              _CardType2(label: card['label'], elevation: card['elevation'])),
         ],
       ),
     );
@@ -64,6 +66,40 @@ class _CardType1 extends StatelessWidget {
             Align(
               alignment: Alignment.bottomLeft,
               child: Text(label),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  const _CardType2({required this.label, required this.elevation});
+
+  final String label;
+  final double elevation;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(color: colors.outline)),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.more_vert_outlined))),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
             )
           ],
         ),

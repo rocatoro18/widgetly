f70dfa88157e20c8158e2cce40b38962c8a7ce1a
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-06 16:02:24 -0700

    Pantallas faltantes

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:widgetly/presentation/screens/animated/animated_screen.dart';
+export 'package:widgetly/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgetly/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgetly/presentation/screens/progress/progress_screen.dart';
+export 'package:widgetly/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgetly/presentation/screens/ui_controls/ui_controls_screen.dart';
 export 'package:widgetly/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgetly/presentation/screens/cards/cards_screen.dart';
 export 'package:widgetly/presentation/screens/home/home_screen.dart';

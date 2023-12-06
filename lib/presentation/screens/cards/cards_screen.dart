4add11e4c3afa1cc5546d3ffea02d164bfddd092
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-06 15:44:26 -0700

    Tarjetas con relleno he imagenes

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -38,6 +38,11 @@ class _CardsView extends StatelessWidget {
               _CardType1(label: card['label'], elevation: card['elevation'])),
           ...cards.map((card) =>
               _CardType2(label: card['label'], elevation: card['elevation'])),
+          ...cards.map((card) =>
+              _CardType3(label: card['label'], elevation: card['elevation'])),
+          ...cards.map((card) =>
+              _CardType4(label: card['label'], elevation: card['elevation'])),
+          const SizedBox(height: 50)
         ],
       ),
     );
@@ -103,6 +108,82 @@ class _CardType2 extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  const _CardType3({required this.label, required this.elevation});
+
+  final String label;
+  final double elevation;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surfaceVariant,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.more_vert_outlined))),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Filled'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  const _CardType4({required this.label, required this.elevation});
+
+  final String label;
+  final double elevation;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      // clipBehavior: Clip.hardEdge, ESTO EVITA QUE LOS HIJOS SE SALGAN DE SU PADRE
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/600/350',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.only(bottomLeft: Radius.circular(20))),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.more_vert_outlined),
+                ),
+              )),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text('$label - Filled'),
+          )
+        ],
       ),
     );
   }

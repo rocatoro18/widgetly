ac8e39bab520f727d88ef57fb8761b4f896534f8
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-06 12:24:41 -0700

    Custom Button

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -63,7 +63,7 @@ class _ButtonsView extends StatelessWidget {
                 icon: const Icon(Icons.ac_unit_sharp),
                 label: const Text('TextButton Icon')),
             // TODO: CUSTOM BUTTON
-
+            const CustomButton(),
             IconButton(
                 onPressed: () {},
                 icon: const Icon(Icons.app_registration_rounded)),
@@ -75,6 +75,31 @@ class _ButtonsView extends StatelessWidget {
                   iconColor: const MaterialStatePropertyAll(Colors.white)),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(
+                'CustomButton',
+                style: TextStyle(color: Colors.white),
+              )),
         ),
       ),
     );

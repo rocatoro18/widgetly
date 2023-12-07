c2c383395ba6f632dcf1ab71ad03c5ae2ad50610
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-06 21:37:30 -0700

    Dialog & License

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
@@ -14,11 +15,48 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
 
+  // BUILDCONTEXT ES LA INFORMACION DE LA APLICACION
+  void openDialog(BuildContext context) {
+    // BUILDER = ALGO QUE SE CONSTRUYE EN TIEMPO DE EJECUCION
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text('¿Estas seguro?'),
+              content: const Text('HOLA OFSDFSDFDSD'),
+              actions: [
+                TextButton(
+                    onPressed: () => context.pop(),
+                    child: const Text('Cancelar')),
+                FilledButton(
+                    onPressed: () => context.pop(),
+                    child: const Text('Aceptar'))
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbar y Dialogos'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+                onPressed: () {
+                  showAboutDialog(
+                      context: context,
+                      children: [const Text('ESTO ES UN MENSAJE')]);
+                },
+                child: const Text('Licencias Usadas')),
+            FilledButton.tonal(
+                onPressed: () => openDialog(context),
+                child: const Text('Mostrar Dialogo de Pantalla'))
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
           icon: const Icon(Icons.remove_red_eye_outlined),

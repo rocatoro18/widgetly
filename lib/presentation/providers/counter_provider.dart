fd1d8017e0acfc05d068cc7458c81489384a1292
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-08 23:27:10 -0700

    Introduccion provider

---
+++ lib/presentation/providers/counter_provider.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// STATEPROVIDER = PROVEDOR DE UN ESTADO (¿PODRIA SER REFERENCIA REACTIVA?)
+final counterProvider = StateProvider((ref) => 5);
--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgetly/presentation/providers/counter_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   const CounterScreen({super.key});
 
   static const name = 'counter_screen';
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // ESTAR PENDIENTE DEL COUNTER PROVIDER
+    final int clickCounter = ref.watch(counterProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
       ),
       body: Center(
-        child: Text('Valor: 10', style: Theme.of(context).textTheme.titleLarge),
+        child: Text('Valor: $clickCounter',
+            style: Theme.of(context).textTheme.titleLarge),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
